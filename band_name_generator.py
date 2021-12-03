04636dbda3c3e930a3422f737b5d6c5324366393
Dima Korshuk <dimakorshuk@Dimas-MacBook-Pro.local>
2021-12-03 09:36:59 +0200

    adding band name generator

---
+++ band_name_generator.py
@@ -0,0 +1,4 @@
+print("welcome to the band name generator.")
+city = input("What city did you grow up in?\n")
+pet_name = input("What is the name of a pet?\n")
+print("Your band name could be " + city + " " + pet_name)
